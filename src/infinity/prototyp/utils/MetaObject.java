b165f50d1292259db3dffc2c263663435c13b2de
Andre Schloemp <andre.schloemp@me.com>
2013-07-11 08:24:58 +0200

    libs

---
+++ src/infinity/prototyp/utils/MetaObject.java
@@ -0,0 +1,11 @@
+package infinity.prototyp.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andre
+ * Date: 11.07.13
+ * Time: 08:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class MetaObject {
+}
