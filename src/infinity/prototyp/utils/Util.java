8db22275c2cd236fdbf3203e6b202d3d02a0c3cf
Andre Schloemp <andre.schloemp@me.com>
2013-07-19 21:53:03 +0200

    next

---
+++ src/infinity/prototyp/utils/Util.java
@@ -0,0 +1,11 @@
+package infinity.prototyp.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andre
+ * Date: 15.07.13
+ * Time: 14:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Util {
+}
